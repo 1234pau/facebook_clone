261f7bceec3f364a76da0c1d6ee74da8987c7697
paul geana <87538456+1234pau@users.noreply.github.com>
2022-10-27 12:18:17 +0300

    finish modal 1

--- src/components/ListModalMenu.js
+++ src/components/ListModalMenu.js
@@ -1,9 +1,67 @@
 import './modals.css'
+import { BsPencilSquare, BsBookFill , BsStarFill, BsMegaphoneFill, BsFillFlagFill, BsPeopleFill,BsHeartFill, BsHandbagFill, BsFillCalendarPlusFill} from "react-icons/bs";
+//import { useState } from 'react';
 
 const ListModalMenu = () => {
+const items = ([
+  {
+    id:1,
+    icon:<BsPencilSquare/>,
+    content:"Post"
+  },
+  {
+    id:2,
+    icon:<BsBookFill />,
+    content:"Story"
+  },
+  {
+    id:3,
+    icon:<BsStarFill/>,
+    content:"Life event"
+  },
+  {
+    id:4,
+    icon:<BsFillFlagFill/>,
+    content:"Page"
+  },
+  {
+    id:5,
+    icon:<BsMegaphoneFill />,
+    content:"Ad"
+  },
+  {
+    id:6,
+    icon:<BsPeopleFill/>,
+    content:"Group"
+  },
+  {
+    id:7,
+    icon:<BsFillCalendarPlusFill/>,
+    content:"Event"
+  },
+  {
+    id:8,
+    icon:<BsHandbagFill />,
+    content:"Marketplace listing"
+  },
+  {
+    id:9,
+    icon:<BsHeartFill/>,
+    content:"Fundraiser"
+  }
+])
+
   return (
     <div className='containerListModalMenu'>
-      list
+      <h3>Create</h3>
+      {items.map((item) =>(
+        <div className='itemList' key={item.id}>
+          <div>
+            {item.icon}
+          </div>
+          <p>{item.content}</p>
+        </div>
+      ))}
     </div>
   )
 }
